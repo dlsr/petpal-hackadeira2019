2e4c7992fc784f1fd204b159188fefde678806ee
dlsr <dlsr@cin.ufpe.br>
2019-11-09 19:16:51 -0300

    MQTT Connection

--- js/demo/ui-control.js
+++ js/demo/ui-control.js
@@ -1,3 +1,8 @@
+var mqtt;
+var reconnectTimeout = 2000;
+var host = "broker.hivemq.com";
+var port = 8000;
+
 $(document).ready(function () {
 
     $("#sendWater").click(function () {
@@ -8,12 +13,34 @@ $(document).ready(function () {
         sendFood();
     });
 
+    
+
     updateBowls();
 
 });
 
 function sendWater() {
     //$('#waterProgressBar').width("50%");
+  
+    MQTTconnect();
+
+}
+
+function onConnect() {
+    
+    message = new Paho.MQTT.Message("Hello World");
+    message.destinationName = "gsf4-servo";
+    console.log("connected");
+    mqtt.send(message);
+}
+
+function MQTTconnect() {
+    console.log('...');
+    mqtt = new Paho.MQTT.Client(host, port, "clientjs");
+
+    var options = { timeout: 3, onSuccess: onConnect, };
+
+    mqtt.connect(options);
 }
 
 function sendFood() {
